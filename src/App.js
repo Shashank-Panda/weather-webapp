ea59bed32bf53556769068260d944c4d13dcfba3
Shashank-Panda <shashankpanda54@gmail.com>
2021-07-15 09:12:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // fetch(`api.openweathermap.org/data/2.5/weather?q={'Delhi'}&appid={'0ee51fb6336e80229ec5a82837f78b1e'}`).then((response)=>{
+  //   console.log(response);
+  // });
+  async function sample(){
+    const data = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=28.38&lon=77.12&appid=0ee51fb6336e80229ec5a82837f78b1e').then((response)=>{
+      return response.json();
+    })
+    console.log(data);
+  }
+  sample();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
